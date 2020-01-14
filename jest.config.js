280ca574e410c0724e85dd08350e24f2ed4c495a
Neto Moura <netomourajs@gmail.com>
2020-01-14 16:57:34 -0300

    test: add storeUser on mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
-  testMatch: ['**/src/**/*.[jt]s?(x)', '**/src/?(*.)+(spec|test).[tj]s?(x)']
+  testMatch: ['**/src/**/*.[jt]s?(x)', '**/src/?(*.)+(spec|test).[tj]s?(x)', '!**/src/main/**']
 }
---
+++ src/domain/create-user-usecase.spec.js
@@ -0,0 +1,5 @@
+describe('Create User', () => {
+  test('Should return 200', () => {
+
+  })
+})
