a81a7659c557c77527e6042ea33f133ea590eca0
Neto Moura <netomourajs@gmail.com>
2020-01-14 14:34:57 -0300

    bugs

---
+++ src/main/config/routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+const fg = require('fast-glob')
+
+module.exports = (app) =>{
+	fg.sync('**/src/main/routes/**.js').forEach(file=> require(`../../../${file}`)(router))
+	app.use('/api',router)
+}
---
+++ src/main/entry.js
@@ -0,0 +1,19 @@
+//  Core
+const express = require('express')
+
+//  Dependencies
+const setupApp = require('./config/setup')
+
+//  Routes
+const setupRoutes = require('./config/routes')
+
+//  Vars
+const app = express()
+
+//  Setups
+setupApp(app)
+setupRoutes(app)
+
+app.listen(3000,()=>{
+	console.log('Server On! -> http://localhost:3000')
+})
